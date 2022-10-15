caff5a810004af0a21284bfe9607f45c4fa0c328
Camilo-Abn <camilovalenzuela@live.cl>
2022-10-15 14:34:15 -0300

    table

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ class PaisSelector {
         function listQ(){
             changedText.textContent = this.value;
             this.valor_actual = this.value;
+            botones();
         }
         document.getElementById(this.id_selector).onchange = listQ;
     }
@@ -25,25 +26,27 @@ var participantes2 = new PaisSelector("pais_mc2", "pais2")
 participantes1.detectaCambios()
 participantes2.detectaCambios()
 
-var xmlhttp = new XMLHttpRequest();
-var url = "data/argentina2.json";
-
-xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    myFunction(myArr);
+function botones() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "data/argentina2.json";
+    
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+        }
+    };
+    
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    
+    function myFunction(arr) {
+        var out = "";
+        var i;
+        for(i = 0; i < arr.length; i++) {
+            nombre = arr[i].PARTICIPANTE.NOMBRE
+            out += '<button class="btn-participante" id='+nombre+'>'+nombre+'</button>';
+        }
+        document.getElementById("partipantes-pais1").innerHTML = out;
     }
-};
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        nombre = arr[i].PARTICIPANTE.NOMBRE
-        out += '<button class="btn-participante" id='+nombre+'>'+nombre+'</button>';
-    }
-    document.getElementById("partipantes-pais1").innerHTML = out;
 }
