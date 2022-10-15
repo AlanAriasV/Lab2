542e738dd8d449ac6a0f33de4aa8e8909dec8728
Camilo-Abn <camilovalenzuela@live.cl>
2022-10-15 04:39:04 -0300

    generate participant btns

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ const chile = '../data/chile.json';
 const espana = '../data/espana.json' 
 const mexico = '../data/mexico.json'; 
 
-var xmlhttp = new XMLHttpRequest();
-
 class PaisSelector {
     constructor(id_selector, id_valor) {
         this.id_selector = id_selector;
@@ -26,5 +24,26 @@ var participantes1 = new PaisSelector("pais_mc1", "pais1")
 var participantes2 = new PaisSelector("pais_mc2", "pais2")
 participantes1.detectaCambios()
 participantes2.detectaCambios()
-//xmlhttp.open("GET", "datos.json", true);
-//xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+var url = "data/argentina2.json";
+
+xmlhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    myFunction(myArr);
+    }
+};
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        nombre = arr[i].PARTICIPANTE.NOMBRE
+        out += '<button class="btn-participante" id='+nombre+'>'+nombre+'</button>';
+    }
+    document.getElementById("partipantes-pais1").innerHTML = out;
+}
